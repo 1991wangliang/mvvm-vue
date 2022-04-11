3ee162c5260ea0fa49fac010450bd97d49c1dfb7
yaowenlu <yaowenlu@laiease.com>
2022-04-11 18:55:05 +0800

    <fix>(eslint+prettier配置)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
     ecmaVersion: 2020
   },
   rules: {
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'never'],
+    'space-before-function-paren': ['error', 'never'],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
